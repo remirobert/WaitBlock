3d984e8fd397634b490ccf3b57363dd3e0280e96
rémi  <remirobert33530@gmail.com>
2015-06-10 21:31:08 +0200

    clean

--- Example/Pods/Target Support Files/Pods/Pods-environment.h
+++ Example/Pods/Target Support Files/Pods/Pods-environment.h
@@ -15,7 +15,7 @@
 // WaitBlock
 #define COCOAPODS_POD_AVAILABLE_WaitBlock
 #define COCOAPODS_VERSION_MAJOR_WaitBlock 0
-#define COCOAPODS_VERSION_MINOR_WaitBlock 2
+#define COCOAPODS_VERSION_MINOR_WaitBlock 3
 #define COCOAPODS_VERSION_PATCH_WaitBlock 0
 
 // 🔒
