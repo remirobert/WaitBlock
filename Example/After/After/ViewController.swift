96607b7f62ecc809f8a1d3f6321583bbdb78f3bf
rémi  <remirobert33530@gmail.com>
2015-06-10 13:51:24 +0200

    add source

---
+++ Example/After/After/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  After
+//
+//  Created by Remi Robert on 10/06/15.
+//  Copyright (c) 2015 Remi Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    @IBAction func addLimit(sender: AnyObject) {
+        Wait🕟Block.executeBlock("test", limitTimer: 3) { () -> () in
+            println("test : \(NSDate().description)")
+        }
+
+        Wait🕟Block.executeBlock("test1", limitTimer: 1) { () -> () in
+            println("test1 : \(NSDate().description)")
+        }
+
+        Wait🕟Block.executeBlock("test2", limitTimer: 0.5) { () -> () in
+            println("test2 : \(NSDate().description)")
+        }
+
+        Wait🕟Block.executeBlock("test3", limitTimer: 6) { () -> () in
+            println("test3 : \(NSDate().description)")
+        }
+    }
+}
+
